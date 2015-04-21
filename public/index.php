cfde70e50870e4ff0be9c1a8a98bc4ebdb406f50
Benny Cardenas <benitocardenas@gmail.com>
2015-04-21 15:23:27 -0500

    Logo and links aligned in middle

--- public/index.php
+++ public/index.php
@@ -3,29 +3,30 @@
 <head>
 	<title></title>
 	<? require_once("../views/partials/htmlHeader.php") ?>
+    <link rel="stylesheet" type="text/css" href="/css/index.css">
 
 </head>
 <body>
 	<? require_once("../views/partials/header.php"); ?>
-	<div id="container">
-     
+    <div id="homepage">
+        <div id="img">
+            <img src="/img/logo.png">
+        </div>	
+        
 
-        <div id="login">
-            <a class="center" href="auth.login.php">Log In</a>
+        <div id="links">
+            <div id="login">
+                <a class="center" href="auth.login.php">Log In</a>
+            </div>
+            <div id="createaccount">
+                <a class="center" href="users.create.php">Create Account</a>
+            </div>
+            <div id="preferences">
+                <a class="center" href="users.edit.php">Preferences</a>
+            </div>
         </div>
-        <br>
-        <div id="createaccount">
-            <a class="center" href="users.create.php">Create Account</a>
-        </div>
-        <br>
-        <div id="preferences">
-            <a class="center" href="users.edit.php">Preferences</a>
-        </div>
-        <br>
-		<div id="content">
+    </div>
 
-		</div>
-	</div>
 	<? require_once("../views/partials/footer.php"); ?>
 </body>
 </html>
