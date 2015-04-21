c28529cd1842192506a15d18cde117efe073d385
Benny Cardenas <benitocardenas@gmail.com>
2015-04-21 09:56:40 -0500

    updated html in files for basic content structure

---
+++ public/ads.edit.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ad Edit</title>
+</head>
+<body>
+    <div id="header"> <? require_once("../views/partials/header.php"); ?> </div>
+    <div id="container">
+        <div id="sidebar"> <? require_once("../views/partials/sidebar.php");  ?> </div>
+
+        <div id="content"></div>
+
+    </div>
+    <div id="footer"> <? require_once("../views/partials/footer.php"); ?> </div>
+</body>
+</html>
