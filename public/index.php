b505b2fabff758105cf0e859a425291516fdd078
Benny Cardenas <benitocardenas@gmail.com>
2015-04-21 13:20:32 -0500

    trying to resolve defferences in branches

--- public/index.php
+++ public/index.php
@@ -2,15 +2,25 @@
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="/css/main.css">
+	<link rel="stylesheet" type="text/css" href="/css/index.css">
 </head>
 <body>
 	<? require_once("../views/partials/header.php"); ?>
 	<div id="container">
-		
+     
 
-        <div></div>
-
+        <div id="login">
+            <a class="center" href="auth.login.php">Log In</a>
+        </div>
+        <br>
+        <div id="createaccount">
+            <a class="center" href="users.create.php">Create Account</a>
+        </div>
+        <br>
+        <div id="preferences">
+            <a class="center" href="users.edit.php">Preferences</a>
+        </div>
+        <br>
 		<div id="content">
 
 		</div>
