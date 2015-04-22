3af1080dcd64ca8ce553d60e4f37949e65b06288
Ryan Dinesman <rdinesman@gmail.com>
2015-04-22 10:21:54 -0500

    created, stylized, and finished header.php

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,5 +1,21 @@
 
 
 <div id="header">
-	header
+	<div id="logoBox">
+		<img id="logo" src="img/Logo.png">
+		<br>
+		<img id="tagline" src="img/Tagline.png">
+	</div>
+
+	<div id="location">
+		San Antonio
+	</div>
+
+	<div id="userUtil">
+		<div id="loginStatus">
+			Logged in as: <span id="loggedInUser">Timmy Teemo</span>
+		</div>
+		<BR>
+		<a href="">Log Out</a>
+	</div>
 </div>
